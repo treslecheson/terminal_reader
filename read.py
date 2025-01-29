68a857679f920bb955ac68f7f367e7ed1786c36f
treslecheson <146301502+treslecheson@users.noreply.github.com>
2025-01-28 18:14:53 -0700

    Add files via upload

---
+++ read.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+
+
+
+
+files = subprocess.run(['ls'], capture_output=True, text=True)
+files = files.stdout.splitlines()
+
+
+files_length = len(files)
+    
+
+while True:
+    try:
+        # Code that might raise a ValueError
+        page = int(input("What page would you like to start on: "))
+        if  0<=page<=files_length:
+            break
+        else:
+            print("Not a page")
+            continue
+
+    except ValueError:
+        # Code to handle the error
+        print("You did not type a number")
+        continue
+
+
+
+while page <= files_length:
+
+    subprocess.run(['termvisage', files[page]])
+    print(f"Pg. {page}")
+    user_input = input("Press q to quit. Press Enter to Continue: ")
+    if user_input == "q":
+        break
+    else:
+        page += 1
+        continue
