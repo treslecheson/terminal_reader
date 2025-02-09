f0cd8b2cbb87316c85ce509ebd830beece5b3956
treslecheson <146301502+treslecheson@users.noreply.github.com>
2025-02-09 12:58:04 -0700

    Update read.py

--- read.py
+++ read.py
@@ -1,37 +1,49 @@
 #!/usr/bin/env python3
 import subprocess
-import os
+from InquirerPy import prompt
 
 
 files = subprocess.run(['ls'], capture_output=True, text=True)
-files = files.stdout.splitlines()
+files2 = files.stdout.splitlines()
 
 
-files_length = len(files)
+
+files_length = len(files2)
     
 
-while True:
-    try:
-        page = int(input("What page would you like to start on: "))
-        if  0<=page<=files_length:
-            break
-        else:
-            print("Not a page")
-            continue
 
-    except ValueError:
-        print("You did not type a number")
-        continue
+starting_question = [
+    {
+        "type": "list",
+        "message": "What page would you like to start on?",
+        "choices": files2,
+    },
+]
 
+result = prompt(starting_question)
+choice = result[0]
 
+start = -1
+for images in files2:
+    start += 1
+    if images == choice:
+        break
 
+page = start
 while page <= files_length:
 
-    subprocess.run(['termvisage', files[page]])
-    print(f"Pg. {page}")
-    user_input = input("Press q to quit. Press Enter to Continue: ")
-    if user_input == "q":
-        break
-    else:
+    subprocess.run(['termvisage', files2[page]])
+    continue_quit_question = [
+        {
+            "type": "list",
+            "message": f"Pg. {page}",
+            "choices": ["Continue", "Quit"],
+        },
+    ]
+    continue_result = prompt(continue_quit_question)
+    continue_choice = continue_result[0]
+    if continue_choice == "Continue":
         page += 1
         continue
+    else:
+        break
